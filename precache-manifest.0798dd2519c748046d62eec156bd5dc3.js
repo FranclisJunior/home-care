d925486eecbbaf09c44f9ba10b84d430b5e93996
Franclis Galdino <franclis.silva@virtus.ufcg.edu.br>
2022-05-05 21:56:22 -0300

    Updates

---
+++ precache-manifest.0798dd2519c748046d62eec156bd5dc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f689b7f18507d660383e021da6353d",
+    "url": "/home-care/index.html"
+  },
+  {
+    "revision": "bc2f3500be44ed670d04",
+    "url": "/home-care/static/css/2.2729d855.chunk.css"
+  },
+  {
+    "revision": "90e920a261d37f81eda0",
+    "url": "/home-care/static/css/main.077b0043.chunk.css"
+  },
+  {
+    "revision": "bc2f3500be44ed670d04",
+    "url": "/home-care/static/js/2.baf237f5.chunk.js"
+  },
+  {
+    "revision": "bc4aedc6b34dfdf097e8dd8dae8ff8cf",
+    "url": "/home-care/static/js/2.baf237f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e920a261d37f81eda0",
+    "url": "/home-care/static/js/main.7ba1eb09.chunk.js"
+  },
+  {
+    "revision": "1e7e71cf562e546e95eb",
+    "url": "/home-care/static/js/runtime-main.b94981e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4a9689e40d6b61cb01b0eb78ac7651e.js"
+  "/home-care/precache-manifest.0798dd2519c748046d62eec156bd5dc3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home-care/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
